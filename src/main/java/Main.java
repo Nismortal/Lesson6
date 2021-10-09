f7439260a1c28a535a25c9faa17406d0a173ee52
m2 <m@ya2.ru>
2021-10-09 14:45:43 +0300

    Init commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+}
